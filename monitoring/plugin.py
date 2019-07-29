867de010573f291aa2ef235848a7660ec0769228
Aleksandr Sobolev <thesobolev@gmail.com>
2019-07-29 13:18:23 +0600

    Subscribe to NewBlockHashesEvent event

--- monitoring/plugin.py
+++ monitoring/plugin.py
@@ -1,15 +1,45 @@
-from argparse import (
-    ArgumentParser,
-    Namespace,
-    _SubParsersAction,
-)
+import argparse
+import asyncio
+import logging
 
-from trinity.config import (
-    TrinityConfig,
-)
-from trinity.extensibility import (
-    BaseMainProcessPlugin,
-)
+from lahja import ConnectionConfig
+
+from trinity.constants import MAIN_EVENTBUS_ENDPOINT
+
+from trinity.config import TrinityConfig
+from trinity.endpoint import TrinityEventBusEndpoint
+from trinity.extensibility.events import PluginStartedEvent
+
+from trinity.extensibility import BaseMainProcessPlugin
+from trinity.protocol.eth.events import NewBlockHashesEvent
+from trinity._utils.os import friendly_filename_or_url
+
+
+async def monitoring(normalized_name, trinity_config) -> None:
+    event_bus = TrinityEventBusEndpoint("monitoring_ui")
+    connection_config = ConnectionConfig.from_name(
+        normalized_name,
+        trinity_config.ipc_dir,
+    )
+    await event_bus.start()
+    await event_bus.start_server(connection_config.path)
+    await event_bus.connect_to_endpoints(
+        ConnectionConfig.from_name(
+            MAIN_EVENTBUS_ENDPOINT, trinity_config.ipc_dir
+        )
+    )
+    await event_bus.announce_endpoint()
+    await event_bus.broadcast(
+        PluginStartedEvent(type(MonitoringPlugin))
+    )
+
+    asyncio.ensure_future(event_bus.auto_connect_new_announced_endpoints())
+    event_bus.subscribe(
+        NewBlockHashesEvent,
+        lambda event: logging.info(
+            event.msg
+        )
+    )
 
 
 class MonitoringPlugin(BaseMainProcessPlugin):
@@ -19,8 +49,8 @@ class MonitoringPlugin(BaseMainProcessPlugin):
 
     @classmethod
     def configure_parser(cls,
-                         arg_parser: ArgumentParser,
-                         subparser: _SubParsersAction) -> None:
+                         arg_parser: argparse.ArgumentParser,
+                         subparser: argparse._SubParsersAction) -> None:
 
         attach_parser = subparser.add_parser(
             'monitoring',
@@ -29,5 +59,10 @@ class MonitoringPlugin(BaseMainProcessPlugin):
         attach_parser.set_defaults(func=cls.run_monitoring)
 
     @classmethod
-    def run_monitoring(cls, args: Namespace, trinity_config: TrinityConfig) -> None:
-        pass
+    def run_monitoring(cls, args: argparse.Namespace, trinity_config: TrinityConfig):
+        normalized_name = friendly_filename_or_url("monitoring_ui")
+        with trinity_config.process_id_file(normalized_name):
+            loop = asyncio.get_event_loop()
+            asyncio.ensure_future(monitoring(normalized_name, trinity_config))
+            loop.run_forever()
+            loop.close()
